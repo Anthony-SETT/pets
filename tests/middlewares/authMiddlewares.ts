56fbdd877faeabaf07daa79d4a6b13d8b66461d0
Anthony <anthonypascoallucas@gmail.com>
2024-10-29 20:34:04 -0300

    middlewares

---
+++ tests/middlewares/authMiddlewares.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
+
+export const securityHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  
+}
+ 
