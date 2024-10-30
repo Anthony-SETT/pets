b19c78af57535bdccbbf06b2db7b3f4df6c9b500
Anthony <anthonypascoallucas@gmail.com>
2024-10-30 19:24:33 -0300

    env auth middlewares

--- tests/middlewares/authMiddlewares.ts
+++ tests/middlewares/authMiddlewares.ts
@@ -6,6 +6,16 @@ export const securityHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  
-}
- 
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    res.status(401).send({ message: "Unauthorized" });
+    return;
+  }
+  jwt.verify(token, process.env.JWT_SECRET as string, (err) => {
+    if (err) {
+      res.status(401).send({ message: "Unauthorized" });
+      return;
+    }
+    next();
+  });
+};
